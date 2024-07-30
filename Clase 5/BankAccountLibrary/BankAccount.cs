a136dd746e094f51c6fbb398ebc3d3746065504c
Marilyn <140780754+DevCodeJMMarilyn@users.noreply.github.com>
2024-07-30 11:59:25 -0600

    Add files via upload

---
+++ Clase 5/BankAccountLibrary/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountLibrary
+{
+    public class BankAccount : IBankAccount
+    {
+        public string AccountOwner { get; set; }
+
+        public string AccountName { get; private set; }
+
+        public decimal Balance { get; private set; }
+
+        public decimal GetBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MakeDeposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void MakeWithdraw(decimal amount)
+        {
+            if((Balance - amount) < 0)
+                throw new NotImplementedException("No hay fondos suficientes");
+            if (amount < 0)
+                throw new NotImplementedException("La cantidad debe ser mayor que cero");
+            
+            Balance -= amount;
+        }
+
+
+
+
+
+        //agregar interfaz para crear metodod
+
+
+
+
+    }
+}
---
+++ Clase 5/BankAccountLibrary/Interface1.cs
@@ -0,0 +1,12 @@
+﻿namespace BankAccountLibrary
+{
+    public interface IBankAccount
+    {
+        void MakeDeposit(decimal amount); //la funcion no necesita retornar con void
+
+        void MakeWithdraw(decimal amount);
+
+        decimal GetBalance(); //la funcion va a retornar un decimal
+
+    }
+}
